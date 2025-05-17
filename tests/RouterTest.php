46fb2ab0f578b2190462a1c21ed085dc6593767b
h3y-retr0 <jgr.espiga@protonmail.com>
2025-05-17 23:18:28 +0200

    Use mock builder for server interface

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -5,17 +5,27 @@ namespace Lune\Tests;
 use Lyra\HttpMethod;
 use Lyra\Request;
 use Lyra\Router;
-use Lyra\Tests\MockServer;
+use Lyra\Server;
 use PHPUnit\Framework\TestCase;
 
 class RouterTest extends TestCase {
+    private function createMockRequest(string $uri, HttpMethod $method): Request {
+        $mockServer = $this->getMockBuilder(Server::class)->getMock();
+        $mockServer->method('requestUri')->willReturn($uri);
+        $mockServer->method('requestMethod')->willReturn($method);
+        $mockServer->method('postData')->willReturn([]);
+        $mockServer->method('queryParams')->willReturn([]);
+
+        return new Request($mockServer);
+    }
+
     public function test_resolve_basic_route_with_callback_action() {
         $uri = '/test';
         $action =  fn () => "test";
         $router = new Router();
         $router->get($uri, $action);
 
-        $route = $router->resolve(new Request(new MockServer($uri, HttpMethod::GET)));
+        $route = $router->resolve($this->createMockRequest($uri, HttpMethod::GET));
         $this->assertEquals($uri, $route->uri());
         $this->assertEquals($action, $route->action());
     }
@@ -35,7 +45,7 @@ class RouterTest extends TestCase {
         }
         
         foreach ($routes as $uri => $action) {
-            $route = $router->resolve(new Request(new MockServer($uri, HttpMethod::GET)));
+            $route = $router->resolve($this->createMockRequest($uri, HttpMethod::GET));
             $this->assertEquals($uri, $route->uri());
             $this->assertEquals($action, $route->action());
         }
@@ -63,7 +73,7 @@ class RouterTest extends TestCase {
         }
 
         foreach ($routes as [$method, $uri, $action]) {
-            $route = $router->resolve(new Request(new MockServer($uri, $method)));
+            $route = $router->resolve($this->createMockRequest($uri, $method));
             $this->assertEquals($uri, $route->uri());
             $this->assertEquals($action, $route->action());
         }
