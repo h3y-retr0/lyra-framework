4e7250a06a1283a9dcf5f80c8a7598c9d4d67302
h3y-retr0 <jgr.espiga@protonmail.com>
2025-06-14 23:28:11 +0200

    Feature login

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,14 @@ use Lyra\Http\Request;
 use Lyra\Http\Response;
 use Lyra\Routing\Route;
 
-Route::get('/', fn ($request) => Response::text(auth()->name));
+Route::get('/', function ($request) {
+    if (isGuest()) {
+        return Response::text('Guest');
+    }
+    
+    return Response::text(auth()->name);
+});
+
 Route::get('/form', fn ($request) => view("form"));
 
 Route::get('/register', fn ($request) => view("auth/register"));
@@ -34,3 +41,31 @@ Route::post('/register', function (Request $request) {
 
     return redirect('/');
 });
+
+Route::get('/login', fn ($request) => view('auth/login'));
+
+Route::post('/login', function (Request $request) {
+    $data = $request->validate([
+        "email" => ["required", "email"],
+        "password" => "required",
+    ]);
+
+    $user = User::firstWhere('email', $data['email']);
+
+    if (is_null($user) || !app(Hasher::class)->verify($data["password"], $user->password)) {
+        return back()->withErrors([
+            'email' => ['email' => 'Credentials are invalid'],
+            'password' => ['password' => 'Credentials are invalid']
+        ]);
+    }
+
+    $user->login();
+
+    return redirect('/');
+});
+
+Route::get('/logout', function ($request) {
+    auth()->logout();
+    return redirect('/');
+});
+
