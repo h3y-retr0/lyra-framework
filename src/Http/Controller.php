42e9c757f55e7c507ff671b67f3aeb8ca822a0eb
h3y-retr0 <jgr.espiga@protonmail.com>
2025-06-16 15:01:13 +0200

    Add middlewares to the Controller class

--- src/Http/Controller.php
+++ src/Http/Controller.php
@@ -3,5 +3,19 @@
 namespace Lyra\Http;
 
 class Controller {
-    //
+    /**
+     * HTTP middlewares.
+     *
+     * @var array
+     */
+    protected array $middlewares = [];
+
+    public function middlewares(): array {
+        return $this->middlewares;
+    }
+
+    public function setMiddlewares(array $middlewares): self {
+        $this->middlewares = array_map(fn ($middleware) => new $middleware(), $middlewares);
+        return $this;
+    }
 }
